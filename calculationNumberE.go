06d097b0c16dfbbae53c53967b7844499e3a1bac
AlfredoCU <alfredo_11_cu@hotmail.com>
2020-09-28 22:50:45 -0500

    Se comprobo que el m´étodo factorial es más preciso para llegar úmero de Euler.

--- calculationNumberE.go
+++ calculationNumberE.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"fmt"
-	"math"
+	// "math"
 )
 
 func main() {
 	// Declaration of variable
 	var e float64
-	//var f float64 = 1
+	var f float64 = 1
 
 	// Calculation of Number e with factorial
-	//for i := 1; i <= 10; i++ {
-	//	f *= float64(i)
-	//	e = 1 / f
-	//}
-
-	// Calculation of Number e
-	for n := 1; n <= 1000000; n++ {
-			e = math.Pow(1 + (1 / float64(n)), float64(n))
+	for i := 0; i < 18; i++ { // 17 Iterations for ê
+		f = 1
+		for j := i; j > 1; j-- {
+			f *= float64(j)
+		}
+		e += 1 / f
 	}
 
+	// Calculation of Number e
+	// for n := 0; n <= 1000000; n++ {
+	//		e = math.Pow(1 + (1 / float64(n)), float64(n))
+	// }
+
 	// Print result
-	fmt.Print(e) // ê = 2.7182804690957534
-				 // e = 2.718281828459045
+	fmt.Print(e)
+	// ê = 2.7182818284590455 // One method
+	// ê = 2.7182804690957534 // Two method
+	// e = 2.718281828459045
 }
